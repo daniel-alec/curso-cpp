fbc0bca01a3b1651fa60ad85d25b8caa666365c3
Daniel Alexandre Cordeiro <daniel.cordeiro@simepar.br>
2021-05-27 01:23:46 -0300

    Pointers
    Signed-off-by: Daniel Alexandre Cordeiro <daniel.cordeiro@simepar.br>

--- src/UniversidadePrj.cpp
+++ src/UniversidadePrj.cpp
@@ -12,6 +12,7 @@ private:
     float nota;
     float nota1;
     float nota2;
+    float notas[2] = {0.0, 0.0};
 
 public:
     // construtor
@@ -94,6 +95,31 @@ public:
         return &media;
     }
 
+    float *getNotas() {
+        return notas;
+    }
+
+    void setNotas(float notas[]) { // *notas ou notas[2]
+        this->notas[0] = notas[0];
+        this->notas[1] = notas[1];
+
+    }
+
+    float calculaMediaNotas() {
+        float media = 0.0f;
+        
+        // for (int i=0; i <= 1; i++) {
+        //     media += notas[i];
+        // }
+        
+        float *p = &notas[0];
+        media = media + *p;
+        p++; // pula para próximo elemento no array
+        media = media + *p;
+
+        return media / 2;
+    }
+
 };
 
 int main() { 
@@ -134,18 +160,36 @@ int main() {
     // cout << "b = " << b << endl;
     // cout << "x = " << x << endl;
 
-    Aluno a1(100, "João");
+    // Aluno a1(100, "João");
     // float notaAluno = 5.5;
     // a1.setNota(notaAluno);
     // cout << a1.getNota() << endl;
     // cout << notaAluno << endl;
-    float n1 = 5.5;
-    float n2 = 7.8;
-    a1.setNota1(n1);
-    a1.setNota2(n2);
-    // cout << a1.calculaMedia() << endl;
-    cout << *(a1.calculaMedia()) << endl; // *(...)  desfaz referência
-    
+    // float n1 = 5.5;
+    // float n2 = 7.8;
+    // a1.setNota1(n1);
+    // a1.setNota2(n2);
+    // // cout << a1.calculaMedia() << endl;
+    // cout << *(a1.calculaMedia()) << endl; // *(...)  desfaz referência
+
+    // int a = 10;
+    // int b = 20;
+    // int *x = &a;
+    // int &y = b;
+    // int *z; // aponta pra NULL
+    // cout << "a = " << a << endl;
+    // cout << "b = " << b << endl;
+    // cout << "x = " << *x << endl;
+    // a = -35;
+    // cout << "a = " << a << endl;
+    // cout << "b = " << b << endl;
+    // cout << "x = " << *x << endl;
+    // x = &b;
+
+    Aluno a(100, "João");
+    float n[2] = {10.0, 8.5};
+    a.setNotas(n);
+    cout << a.calculaMediaNotas() << endl;    
 
     return 0;
 }
